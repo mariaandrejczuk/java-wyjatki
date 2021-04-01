743fcb46f7727b1332ab7b76b35084c651dc5d00
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2021-04-01 07:53:20 +0200

    #5 obsługa wyjątków oznaczonego i nieoznaczonego

--- src/pl/mariaandrejczuk/exceptions/FirstException.java
+++ src/pl/mariaandrejczuk/exceptions/FirstException.java
@@ -1,6 +1,7 @@
 package pl.mariaandrejczuk.exceptions;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class FirstException {
     public static void main(String[] args) {
@@ -8,11 +9,18 @@ public class FirstException {
         int b = 0;
 
         // wyjątek nieoznaczony
-        int result = a / b;
-        result += 10;
-        System.out.println(result);
-
+        try {
+            int result = a / b;
+            result += 10;
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Nie dzielimy przez 0.");
+        }
         //wyjątek oznaczony
-        FileInputStream fileInputStream = new FileInputStream("/to/jest/sciezka/plik.txt");
+        try {
+            FileInputStream fileInputStream = new FileInputStream("/to/jest/sciezka/plik.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("Taki plik nie istnieje.");
+        }
     }
 }
