222f312ec8a57e19b687f3935285bdc3c7f6e652
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2021-04-01 08:12:04 +0200

    #5 wiele instrukcji catch

--- src/pl/mariaandrejczuk/exceptions/FirstException.java
+++ src/pl/mariaandrejczuk/exceptions/FirstException.java
@@ -2,6 +2,7 @@ package pl.mariaandrejczuk.exceptions;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FirstException {
     public static void main(String[] args) {
@@ -21,6 +22,26 @@ public class FirstException {
             FileInputStream fileInputStream = new FileInputStream("/to/jest/sciezka/plik.txt");
         } catch (FileNotFoundException e) {
             System.out.println("Taki plik nie istnieje.");
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("Insert number 1: ");
+            a = scanner.nextInt();
+            System.out.println("Insert number 2: ");
+            b = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println(a / b);
+
+            System.out.print("Insert path to file: ");
+            String path = scanner.nextLine();
+            FileInputStream fileInputStream = new FileInputStream(path);
+
+        }catch(FileNotFoundException e){
+            System.out.println("Does not exists");
+        }catch(ArithmeticException e){
+            System.out.println("Do not divide by 0.");
         }
     }
 }
